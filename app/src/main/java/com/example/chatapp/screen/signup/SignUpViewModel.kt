95a3f8555df0c45f7a89ccf1b3f01e1bbf978d05
Passant-Hatem <79157384+Passant-Hatem@users.noreply.github.com>
2022-09-18 03:29:18 +0200

    remove api key

--- app/src/main/java/com/example/chatapp/screen/signup/SignUpViewModel.kt
+++ app/src/main/java/com/example/chatapp/screen/signup/SignUpViewModel.kt
@@ -1,3 +1,14 @@
 package com.example.chatapp.screen.signup
-class SignUpViewModel  {
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.getstream.chat.android.client.ChatClient
+import io.getstream.chat.android.client.models.User
+import javax.inject.Inject
+
+@HiltViewModel
+class SignUpViewModel @Inject constructor(
+    private val client: ChatClient
+) : ViewModel() {
+
 }
